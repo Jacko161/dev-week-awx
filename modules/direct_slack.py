eac1afe1eda7e49944e2ed1bccd3ce66ebf6bde0
Jackson Fleming <jacksonrobertfleming161@gmail.com>
2021-09-01 07:43:30 +1000

    Fix bug

--- modules/direct_slack.py
+++ modules/direct_slack.py
@@ -29,25 +29,20 @@ def run_module():
         message=dict(type='str', required=True)
     )
 
-    # seed the result dict in the object
-    # we primarily care about changed and state
-    # changed is if this module effectively modified the target
-    # state will include any data that you want your module to pass back
-    # for consumption, for example, in a subsequent task
     result = dict(
         changed=False,
         original_message='',
         message=''
     )
 
-    # the AnsibleModule object will be our abstraction working with Ansible
-    # this includes instantiation, a couple of common attr would be the
-    # args/params passed to the execution, as well as if the module
-    # supports check mode
+    module = AnsibleModule(
+            argument_spec=module_args,
+            supports_check_mode=False
+        )
 
-    # if the user is working with this module in only check mode we do not
-    # want to make any changes to the environment, just return the current
-    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
     email = module.params['email']
     token = module.params['token']
     message = module.params['message']
@@ -64,8 +59,6 @@ def run_module():
 
     post_message_to_slack(message, token, user_id)
     module.exit_json(**result)
-
-
 
 def main():
     run_module()
